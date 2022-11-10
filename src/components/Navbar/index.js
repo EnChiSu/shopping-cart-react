40e4ff073ee8d245848ad5bf848821965f259d41
EnChiSu <r07741074@ntu.edu.tw>
2022-11-10 17:52:23 +0800

    add navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+const Navbar = () => {
+    return (
+        <div className='nav__bar'>
+            <div className='nav__bar_text'>
+                街口攻城獅官方商城 
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
